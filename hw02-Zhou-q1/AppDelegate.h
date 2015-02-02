808b49a75ee5d2cee68512a712ffc6e3dd9faa06
Jiyang <jiyang@JiyangdeMacBook-Pro.local>
2015-02-02 11:38:35 -0700

    Initial Commit

---
+++ hw02-Zhou-q1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hw02-Zhou-q1
+//
+//  Created by Jiyang on 15/2/2.
+//  Copyright (c) 2015年 Jiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hw02-Zhou-q1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hw02-Zhou-q1
+//
+//  Created by Jiyang on 15/2/2.
+//  Copyright (c) 2015年 Jiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
